0cc0a31c19fc00c5d6af061203fae5e2e33d0801
Chris Antaki <ChrisAntaki@gmail.com>
2015-03-03 11:57:53 -0800

    Removed IDL banner

--- homepage/js/homepage.js
+++ homepage/js/homepage.js
@@ -100,21 +100,6 @@
 
 
 
-// Internet Defense League
-(function(){
-    window._idl = {};
-    _idl.variant = "modal";
-    (function() {
-        var idl = document.createElement('script');
-        idl.type = 'text/javascript';
-        idl.async = true;
-        idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
-        document.querySelector('body').appendChild(idl);
-    })();
-})();
-
-
-
 /*
  * Modules
  */
